087eda652bc457c173618d61852b24a270179c35
Helou Romain <helouromain@gmail.com>
2018-07-29 16:51:54 +0200

    Array

--- js/cours.js
+++ js/cours.js
@@ -1,81 +1,9 @@
-var Personnage = {
-    // Initialise le personnage
-    initPerso: function (nom, sante, force) {
-        this.nom = nom;
-        this.sante = sante;
-        this.force = force;
-    },
-    // Attaque un personnage cible
-    attaquer: function (cible) {
-        if (this.sante > 0) {
-            var degats = this.force;
-            console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
-            cible.sante = cible.sante - degats;
-            if (cible.sante > 0) {
-                console.log(cible.nom + " a encore " + cible.sante + " points de vie");
-            } else {
-                cible.sante = 0;
-                console.log(cible.nom + " est mort !");
-            }
-        } else {
-            console.log(this.nom + " ne peut pas attaquer : il est mort...");
-        }
-    }
-};
+var films = ["Le loup de Wall Street", "Vice-Versa", "Babysitting"];
 
-var Joueur = Object.create(Personnage);
-// Initialise le joueur
-Joueur.initJoueur = function (nom, sante, force) {
-    this.initPerso(nom, sante, force);
-    this.xp = 0;
-};
-// Renvoie la description du joueur
-Joueur.decrire = function () {
-    var description = this.nom + " a " + this.sante + " points de vie, " +
-        this.force + " en force et " + this.xp + " points d'expérience";
-    return description;
-};
-// Combat un adversaire
-Joueur.combattre = function (adversaire) {
-    this.attaquer(adversaire);
-    if (adversaire.sante === 0) {
-        console.log(this.nom + " a tué " + adversaire.nom + " et gagne " +
-            adversaire.valeur + " points d'expérience");
-        this.xp += adversaire.valeur;
-    }
-};
+for (var i = 0; i < films.length; i++) {
+    console.log(films[i]);
+}
 
-var Adversaire = Object.create(Personnage);
-// Initialise l'adversaire
-Adversaire.initAdversaire = function (nom, sante, force, race, valeur) {
-    this.initPerso(nom, sante, force);
-    this.race = race;
-    this.valeur = valeur;
-};
-
-
-// ...
-
-var joueur1 = Object.create(Joueur);
-joueur1.initJoueur("Aurora", 150, 25);
-
-var joueur2 = Object.create(Joueur);
-joueur2.initJoueur("Glacius", 130, 30);
-
-console.log("Bienvenue dans ce jeu d'aventure ! Voici nos courageux héros :");
-console.log(joueur1.decrire());
-console.log(joueur2.decrire());
-
-var monstre = Object.create(Adversaire);
-monstre.initAdversaire("ZogZog", 40, 20, "orc", 10);
-
-console.log("Un affreux monstre arrive : c'est un " + monstre.race + " nommé " + monstre.nom);
-
-monstre.attaquer(joueur1);
-monstre.attaquer(joueur2);
-
-joueur1.combattre(monstre);
-joueur2.combattre(monstre);
-
-console.log(joueur1.decrire());
-console.log(joueur2.decrire());
+films.forEach(function (film) {
+    console.log(film);
+});
