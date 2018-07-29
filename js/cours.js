2bca31ee2dcbd4cf7132ca017632b1f97f993a2f
Helou Romain <helouromain@gmail.com>
2018-07-29 13:04:47 +0200

    String Concatenation

--- js/cours.js
+++ js/cours.js
@@ -2,4 +2,6 @@ console.log("Bonjour en JavaScript !");
 console.log("Faisons quelques calculs.");
 console.log(4 + 7);
 console.log(12 / 0);
+console.log("Concatenation de chaine");
+console.log("Conca"+"tenation"+" Example");
 console.log("Au revoir !");
