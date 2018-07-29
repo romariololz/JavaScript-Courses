a0f8ba3e683a689abdbd6602cb3be95db2a67abc
Helou Romain <helouromain@gmail.com>
2018-07-29 16:28:56 +0200

    Strings and Chars Manipulations

--- js/cours.js
+++ js/cours.js
@@ -1,7 +1,23 @@
-function direBonjour(prenom, nom) {
-    var message = "Bonjour, " + prenom + " " + nom + " !";
-    return message;
-}
+console.log("Je suis une chaîne".length);
 
-console.log(direBonjour("Baptiste", "DUPONT"));
-console.log(direBonjour("Sophie", "GONZALVES"));
+var motInitial = "Bora-Bora";
+var motEnMinuscules = motInitial.toLowerCase();
+console.log(motEnMinuscules); // Affiche "bora-bora"
+var motEnMajuscules = motInitial.toUpperCase();
+console.log(motEnMajuscules); // Affiche "BORA-BORA"
+
+var chaine = "azerty";
+console.log(chaine === "azerty"); // Affiche true
+console.log(chaine === "qwerty"); // Affiche false
+
+var sport = "Tennis-ballon"; // 13 caractères
+console.log(sport.charAt(0)); // Affiche "T"
+console.log(sport[0]); // Affiche "T"
+console.log(sport.charAt(6)); // Affiche "-"
+console.log(sport[6]); // Affiche "-"
+
+var prenom = "Odile";
+
+for (var i = 0; i < prenom.length; i++) {
+    console.log(prenom[i]);
+}
