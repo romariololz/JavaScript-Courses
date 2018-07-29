b016ca798191536a7d4d498b724558e73cd98d93
Helou Romain <helouromain@gmail.com>
2018-07-29 13:28:10 +0200

    [If / Else if / Else] test with number prompt

--- js/cours.js
+++ js/cours.js
@@ -1,31 +1,15 @@
-// console.log("Bonjour en JavaScript !");
-// console.log("Faisons quelques calculs.");
-// console.log(4 + 7);
-// console.log(12 / 0);
-// console.log("Concatenation de chaine");
-// console.log("Conca"+"tenation"+" Example");
-// console.log("Au revoir !");
+var nombre = Number(prompt("Entrez un nombre :"));
 
-var a = 3.14;
-console.log("a = " + a);
+if (nombre > 0) {
+    console.log(nombre + " est positif");
+}
+else if (nombre < 0){
+    console.log(nombre + " est négatif");
+}
+else{
+    console.log(nombre + " est nul");
+}
 
-var b = 0;
-console.log("b = " + b);
-b += 1;
-console.log("b = " + b);
-b++;
-console.log("b = " + b);
-
-var e = 3 + 2 * 4;
-console.log("e = " + e);
-
-e = (3 + 2) * 4;
-console.log("e = " + e);
-
-var h = "5";
-console.log(h + 1); // Concaténation : affiche la chaîne "51"
-h = Number("5");
-console.log(h + 1); // Addition numérique : affiche le nombre 6
-
-var prenom = prompt('Entrez votre pseudo');
-alert("Bonjour" + prenom);
+if ((nombre >= 0) && (nombre <= 100)) {
+    console.log(nombre + " est compris entre 0 et 100");
+}
