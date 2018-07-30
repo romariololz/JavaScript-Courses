11cb3b1b5dbb429483a06d982a3239f22252d063
Helou Romain <helouromain@gmail.com>
2018-07-30 17:36:10 +0200

    QuerySelector

--- course_2/js/cours.js
+++ course_2/js/cours.js
@@ -1,5 +1,13 @@
-if (document.body.nodeType === document.ELEMENT_NODE) {
-    console.log("Body est un noeud élément");
-} else {
-    console.log("Body est un noeud textuel");
-}
+// Tous les paragraphes
+console.log(document.querySelectorAll("p").length); // Affiche 3
+
+// Tous les paragraphes à l'intérieur de l'élément identifié par "contenu"
+console.log(document.querySelectorAll("#contenu p").length); // Affiche 2
+
+// Tous les éléments ayant la classe "existe"
+console.log(document.querySelectorAll(".existe").length); // Affiche 8tsByClassName("existe").length); // Affiche 1
+
+// Tous les éléments fils de l'élément identifié par "antiques" ayant la classe "existe"
+console.log(document.querySelectorAll("#antiques > .existe").length); // Affiche 1
+
+console.log(document.querySelector("p"));
