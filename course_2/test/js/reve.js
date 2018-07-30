90b6ed8ee586a09b59cf1170f87166cc93224a3e
Helou Romain <helouromain@gmail.com>
2018-07-30 18:20:35 +0200

    Tests on course_2

---
+++ course_2/test/js/reve.js
@@ -0,0 +1,8 @@
+function compterElements(string) {
+    console.log(document.querySelectorAll(string).length);
+}
+
+console.log(compterElements("p")); // Doit afficher 4
+console.log(compterElements(".adjectif")); // Doit afficher 3
+console.log(compterElements("p .adjectif")); // Doit afficher 3
+console.log(compterElements("p > .adjectif")); // Doit afficher 2
